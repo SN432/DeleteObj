b82b3565fc6a854a2c945a8b334f6b57b77119b0
Your Name <you@example.com>
2022-09-01 11:49:27 +0700

    Changed

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-
 let product = [
-    {id: 1,name: 'car',price: 200},
-    {id: 2,name: 'bike',price: 100},
-    {id: 3,name: 'moto',price: 200}
+  { id: 1, name: "car", price: 200 },
+  { id: 2, name: "bike", price: 100 },
+  { id: 3, name: "moto", price: 200 },
+  { id: 4, name: "airplane", price: 200 },
+  { id: 5, name: "truck", price: 100 },
+  { id: 6, name: "ambulance", price: 200 },
 ];
-
+console.log("All products");
 console.log(product);
+console.log("Length of product");
 console.log(product.length);
 
-function remove(i){
-    for(k=0; k<product.length;k++){
-       if(i !== k ){
-        console.log(product[k])
-        alert(product[i].name+ " is deleted")
-       }
-    }
+function deleteArray(k) {
+  let del = product[k].name;
+  product.splice(k, 1);
+  console.log("All Products after deleting");
+  console.log(product);
+  console.log(del + " is deleted");
 }
-remove(1);
+
+deleteArray(0);
+
+function remove(i) {
+  for (k = 0; k < product.length; k++) {
+    if (i !== k) {
+      console.log(product[k]);
+      alert(product[i].name + " is deleted");
+    }
+  }
+}
+// remove(1);
